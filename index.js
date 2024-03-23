f289ee1fec86258ae794b08bc52830ecf2864c10
Thameera Senanayaka <me@thameera.com>
2024-03-23 23:56:45 +1100

    Run prettier

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { handler as handleTickets } from './lib/tickets/index.js';
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { handler as handleTickets } from "./lib/tickets/index.js";
 
 const main = async () => {
   await yargs(hideBin(process.argv))
-    .command('tickets <action> [options]', "Execute a command", (yargs) => {
-      yargs.positional('action', {
-        type: 'string',
-        describe: 'Action',
-      }).option('u', {
-        alias: 'username',
-        describe: 'Username',
-        type: 'string',
-        demandOption: true,
-      })
-    }, async (argv) => {
-      await handleTickets(argv);
-    })
+    .command(
+      "tickets <action> [options]",
+      "Execute a command",
+      (yargs) => {
+        yargs
+          .positional("action", {
+            type: "string",
+            describe: "Action",
+          })
+          .option("u", {
+            alias: "username",
+            describe: "Username",
+            type: "string",
+            demandOption: true,
+          });
+      },
+      async (argv) => {
+        await handleTickets(argv);
+      },
+    )
     .help()
     .parse();
 
-  console.log('Exiting')
-}
+  console.log("Exiting");
+};
 
 main();
